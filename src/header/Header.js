eb70e6d6c1c040702b03c91288f2adb231fd541a
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-09-16 08:45:44 -0500

    bank

--- src/header/Header.js
+++ src/header/Header.js
@@ -6,6 +6,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,11 +17,15 @@ const useStyles = makeStyles((theme) => ({
   },
   button: {
     color: "white",
-    fontSize: "200px",
-    fontWeight:"bold",
+    fontSize: "10px",
+    fontWeight: "bold",
   },
   title: {
     flexGrow: 1,
+    fontSize: "15px",
+  },
+  spacer: {
+    paddingLeft: "300px",
   },
 }));
 
@@ -31,12 +36,22 @@ export default function Header() {
       <AppBar position="static">
         <Toolbar variant="dense">
           <Link to="/">
-            <img src={logo} alt="logo" />
+            <img width="40%" src={logo} alt="logo" />
           </Link>
           <Typography variant="h6" className={classes.title}>
             MTD Bank
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button className={classes.button}>About Us </Button>
+          <Button className={classes.button}>Product </Button>
+          <Button className={classes.button}>Package </Button>
+          <Button className={classes.button}>Contact </Button>
+          <div className={classes.spacer}></div>
+          <Link to="/login" className="header__link ">
+            <div className="header__option">
+              <span className="header__lineOne">Hello</span>
+              <span className="header__lineTwo">Sign In</span>
+            </div>
+          </Link>
         </Toolbar>
       </AppBar>
     </div>
