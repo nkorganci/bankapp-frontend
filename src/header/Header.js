3a229107f88929846895e6c5ad18cb5feefb49d9
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-09-18 13:30:13 -0500

    a

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,26 +1,49 @@
 import React from "react";
-import { Container, Nav} from "react-bootstrap";
+import eader from "./Header.css";
+import { Container, Nav, Row, Col } from "react-bootstrap";
+import { useHistory } from "react-router";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header-container">
       <Container fluid>
-
-      <Nav variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">Active</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="disabled" disabled>
-      Disabled
-    </Nav.Link>
-  </Nav.Item>
-</Nav>
-
+        <Container className="nav-top">
+          <Row className="row">
+            <Col className="col">
+              <TopNav />
+            </Col>
+          </Row>
+        </Container>
       </Container>
+    </div>
+  );
+};
+
+const TopNav = () => {
+  const history = useHistory();
+  const handleNavClick = (eventKey) => {
+    history.push(`/${eventKey}`);
+  };
+  return (
+    <div>
+      <Nav
+        variant="tabs"
+        defaultActiveKey="advantage"
+        onSelect={handleNavClick}
+      >
+        <Nav.Item>
+          <Nav.Link eventKey="advantage">Advantage</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="Mobile">Mobile</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="signin">Sign In</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="register">Register</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 };
