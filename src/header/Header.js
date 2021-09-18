c2e2d8d25c6ebe1f1db813aeba484d74668572c3
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-09-18 11:07:37 -0500

    a

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,61 +1,28 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import logo from "../images/logo.png";
-import { Link } from "react-router-dom";
-import "./Header.css";
+import { Container, Nav} from "react-bootstrap";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  button: {
-    color: "white",
-    fontSize: "10px",
-    fontWeight: "bold",
-  },
-  title: {
-    flexGrow: 1,
-    fontSize: "15px",
-  },
-  spacer: {
-    paddingLeft: "300px",
-  },
-}));
-
-export default function Header() {
-  const classes = useStyles();
+const Header = () => {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Link to="/">
-            <img width="40%" src={logo} alt="logo" />
-          </Link>
-          <Typography variant="h6" className={classes.title}>
-            MTD Bank
-          </Typography>
-          <Button className={classes.button}>About Us </Button>
-          <Button className={classes.button}>Product </Button>
-          <Button className={classes.button}>Package </Button>
-          <Button className={classes.button}>Contact </Button>
-          <div className={classes.spacer}>
-                  <Link to="/login" className="header__link ">
-            <div className="header__option">
-              <span className="header__lineOne">Hello</span>
-              <span className="header__lineTwo">Sign In</span>
-            </div>
-          </Link>
-          </div>
-    
-        </Toolbar>
-      </AppBar>
+    <div>
+      <Container fluid>
+
+      <Nav variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
+
+      </Container>
     </div>
   );
-}
+};
+
+export default Header;
